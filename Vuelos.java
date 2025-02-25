d45d0df60a03c8c8036b33f516fc0ac3aa08a538
Esbirrusy11 <raumarpar@alu.edu.gva.es>
2025-02-25 16:44:25 +0100

    Vuelos.java

--- Vuelos.java
+++ Vuelos.java
@@ -61,35 +61,21 @@ public class Vuelos {
     }
 
     public void nuevoVuelo(){
-        Scanner sc =new Scanner(System.in);
-        boolean boo=true;
+        Scanner sc = new Scanner(System.in);
         gestionVuelos();
 
-        while (boo){
-            System.out.println("¿Cuál es el número del vuelo?");
-            String numero=sc.nextLine();
-            for (int i = 0; i < numero.length(); i++) {
-                if (!Character.isDigit(numero.charAt(i))) {
-                    System.out.println("Error el número del vuelo no puede contener letras");
-                    System.out.println("¿Cuál es el número del vuelo?");
-                    numero=sc.nextLine();
-                }if (Character.isDigit(numero.charAt(i))){
-                    System.out.println("Vuelo correcto");
-                    boo=false;
-                    break;
-                }
-            }
-        }
-
+        System.out.println("¿Cuál es el número del vuelo?");
+        String numero = sc.nextLine();
         System.out.println("¿Cuál es el origen del vuelo?");
-        String origen=sc.nextLine();
+        String origen = sc.nextLine();
         System.out.println("¿Cuál es el destino del vuelo?");
-        String destino=sc.nextLine();
-        System.out.println("¿Que día sale el vuelo?");
-        String dia=sc.nextLine();
-        System.out.println("¿Que clase es el vuelo?");
-        String clase=sc.nextLine();
-        vuelos.add(new Vuelos(numero,origen,destino,dia,clase));
+        String destino = sc.nextLine();
+        System.out.println("¿Qué día sale el vuelo?");
+        String dia = sc.nextLine();
+        System.out.println("¿Qué clase es el vuelo?");
+        String clase = sc.nextLine();
+
+        vuelos.add(new Vuelos(numero, origen, destino, dia, clase));
         System.out.println("El vuelo fue añadido con éxito");
     }
 
@@ -117,49 +103,69 @@ public class Vuelos {
 
     public void BuscarVuelosClave(String clave){
         Scanner sc=new Scanner(System.in);
-       if (clave.equalsIgnoreCase("Numero")){
-           System.out.println("Valor: ");
-           String valor= sc.next();
-           if (vuelos.contains(buscarVuelos(valor))) {
-               System.out.println(buscarVuelos(valor));
-               System.out.println();
-           }else {
-               System.out.println("El vuelo no existe en la lista");
-           }
-       }else if (clave.equalsIgnoreCase("Origen")){
-           System.out.println("Valor: ");
-           String valor= sc.next();
-           for (Vuelos conta:vuelos){
-               if (conta.getOrigen().equalsIgnoreCase(valor)){
-                   System.out.println(conta);
-               }
-           }
-       }else if (clave.equalsIgnoreCase("Destino")){
-           System.out.println("Valor: ");
-           String valor= sc.next();
-           for (Vuelos conta:vuelos){
-               if (conta.getDestino().equalsIgnoreCase(valor)){
-                   System.out.println(conta);
-               }
-           }
-       }else if (clave.equalsIgnoreCase("Dia")){
-           System.out.println("Valor: ");
-           String valor= sc.next();
-           for (Vuelos conta:vuelos){
-               if (conta.getDia().equalsIgnoreCase(valor)){
-                   System.out.println(conta);
-               }
-           }
-       }else if (clave.equalsIgnoreCase("Clase")){
-           System.out.println("Valor: ");
-           String valor= sc.next();
-           for (Vuelos conta:vuelos){
-               if (conta.getClase().equalsIgnoreCase(valor)){
-                   System.out.println(conta);
-               }
-           }
-       }else {
-           System.out.println("La clave no existe");
-       }
+        if (clave.equalsIgnoreCase("Numero")){
+            System.out.println("Valor: ");
+            String valor= sc.next();
+            if (vuelos.contains(buscarVuelos(valor))) {
+                System.out.println(buscarVuelos(valor));
+                System.out.println();
+            }else {
+                System.out.println("El vuelo no existe en la lista");
+            }
+        }else if (clave.equalsIgnoreCase("Origen")){
+            System.out.println("Valor: ");
+            String valor= sc.next();
+            boolean correcto = false;
+            for (Vuelos conta:vuelos){
+                if (conta.getOrigen().equalsIgnoreCase(valor)){
+                    System.out.println(conta);
+                    correcto=true;
+                }
+            }
+            if (!correcto){
+                System.out.println("El origen ingresado no está en la lista");
+            }
+        }else if (clave.equalsIgnoreCase("Destino")){
+            System.out.println("Valor: ");
+            String valor= sc.next();
+            boolean correcto = false;
+            for (Vuelos conta:vuelos){
+                if (conta.getDestino().equalsIgnoreCase(valor)){
+                    System.out.println(conta);
+                    correcto=true;
+                }
+            }
+            if (!correcto){
+                System.out.println("El destino ingresado no está en la lista de vuelos");
+            }
+        }else if (clave.equalsIgnoreCase("Dia")) {
+            System.out.println("Valor: ");
+            String valor = sc.next();
+            boolean correcto = false;
+            for (Vuelos conta : vuelos) {
+                if (conta.getDia().equalsIgnoreCase(valor)) {
+                    System.out.println(conta);
+                    correcto = true;
+                }
+            }
+            if (!correcto) {
+                System.out.println("El dia ingresado no está en la lista de vuelos");
+            }
+        }else if (clave.equalsIgnoreCase("Clase")){
+            System.out.println("Valor: ");
+            String valor= sc.next();
+            boolean correcto = false;
+            for (Vuelos conta:vuelos){
+                if (conta.getClase().equalsIgnoreCase(valor)){
+                    System.out.println(conta);
+                    correcto=true;
+                }
+            }
+            if (!correcto){
+                System.out.println("La clase ingresada no está en la lista de vuelos");
+            }
+        }else {
+            System.out.println("La clave no existe");
+        }
     }
 }
